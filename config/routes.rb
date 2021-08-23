489d1983199e71cdea0ea39b7c68385bfe8ca62b
ryota-sb <ryota.sb.1206@gmail.com>
2021-08-23 20:53:42 +0900

    ネストルーティングに変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'homes#top'
   
   devise_for :users
-  resources :shops, only: [:index, :show]
+  resources :shops, only: [:index, :show] do
+    resources :reviews, only: [:index, :create]
+  end
 end
